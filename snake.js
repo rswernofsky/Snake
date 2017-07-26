7b329f56830e761f8f4b9e925081fb20ccc7cd42
rswernofsky <Edward@Rebeccas-MacBook-Air-2.local>
2017-07-25 20:17:03 -0700

    fixed bugs

--- snake.js
+++ snake.js
@@ -1,10 +1,26 @@
 let canvas, context, frames;
 
+const size = 20;
+const gridSize = 15;
+
 class Coord {
   constructor(x, y, color) {
     this.x = x;
     this.y = y;
     this.color = color;
+  }
+
+  equals(coord) {
+    return (coord.x === this.x && coord.y === this.y);
+  }
+
+  draw() {
+    context.fillRect(this.x * size, this.y * size, size, size);
+  }
+
+  static random() { // lowercase is good notation 
+    return new Coord(getRandomInt(gridSize), 
+                      getRandomInt(gridSize));
   }
 }
 // each coord will make a square that has a width and height of 20
@@ -12,12 +28,10 @@ class Coord {
 const getRandomInt = (upTo) => Math.floor(upTo * Math.random());
 const getRandomColor = () => '#' + Math.floor((1<<24) * Math.random()).toString(16);
 
-let snake = [new Coord(getRandomInt(15), getRandomInt(15))]; 
+let snake = [Coord.random()];
 // list of the "coordinates" of the squares which will make up the snake shape. Will get multiplied to find the x and y locations
 let fruit = new Coord(1, 1);
 let gameover = false;
-const size = 20;
-const gridSize = 15;
 
 const loop = () => {
   // window.requestAnimationFrame(loop); //runs every animation frame
@@ -26,23 +40,24 @@ const loop = () => {
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   context.fillStyle = '#ff0000';
-  drawSquare(fruit.x, fruit.y);
+  fruit.draw();
 
   context.fillStyle = '#ffffff';
-  drawSquare(snake[0].x, snake[0].y);
+  snake[0].draw();
   context.fillStyle = '#d3d3d3';
   for (let i = 1; i < snake.length; i++) {
-    drawSquare(snake[i].x, snake[i].y);
+    snake[i].draw();
   }
   // draws the snake and the fruit
 
   let head = new Coord(snake[0].x, snake[0].y);
+  lastDir = direction;
+
   switch (direction) {
-    case 2: head.y--; break; // up
-    case 4: head.y++; break; // down
-    case 1: head.x--; break; // left
-    case 3: head.x++; break; // right
-    default: break;
+    case 1: head.y--; break; // up
+    case 3: head.y++; break; // down
+    case 0: head.x--; break; // left
+    case 2: head.x++; break; // right
   }
   // changes the next head according to the direction
 
@@ -51,13 +66,6 @@ const loop = () => {
   if (head.y >= gridSize) head.y = 0;
   if (head.y < 0) head.y = gridSize;
   // loop onto the other side if head goes off the grid
-
-  if (head.x === fruit.x && head.y === fruit.y) {
-    snake[snake.length] = snake[snake.length - 1];
-    fruit.x = getRandomInt(15);
-    fruit.y = getRandomInt(15);
-  }
-  // when the head runs into fruit
 
   for (i = 1; i < snake.length; i++) {
     if (head.x === snake[i].x && head.y === snake[i].y) {
@@ -69,40 +77,54 @@ const loop = () => {
 
   shiftUp();
   snake[0] = head;
+
+  if (head.equals(fruit)) {
+    snake[snake.length] = snake[snake.length - 1];
+    // will return true if any of the pieces of the snake are the same as the fruit
+    while (snake.some((piece) => piece.equals(fruit))) { 
+      fruit = Coord.random();
+    }
+  }
+  // when the head runs into fruit
+
 };
 
-const drawSquare = (x, y) => context.fillRect(x * size, y * size, size, size);
-// shifts the snake
 const shiftUp = () => {
-  for (let i = snake.length - 2; i >= 0; i--) snake[i + 1] = snake[i];
-}
+  for (let i = snake.length - 2; i >= 0; i--) {
+    snake[i + 1] = snake[i];
+  }
+};
 
 const keyMap = {
-  38: 2,  // up
-  40: 4,  // down
-  37: 1,  // left
-  39: 3   // right
+  38: 1,  // up
+  40: 3,  // down
+  37: 0,  // left
+  39: 2   // right
 };
 
-let direction = 0; // not a real direction
+let direction = undefined; // not a real direction
+let lastDir = direction;
 
 document.onkeydown = (e) => {
-  if (isOppositeDir(direction, keyMap[e.keyCode])) return;
-  direction = keyMap[e.keyCode] || direction;
-  if (e.keyCode === 32) window.stopAnim();
-}
+  if (e.keyCode === 32) stopAnimation();
+  const newDir = keyMap[e.keyCode];
+  if (isOppositeDir(lastDir, newDir) || 
+      newDir === undefined) return;
+  direction = newDir;
+  // if the keyMap isn't undefined, it will return it without looking at direction
+};
 
 const isOppositeDir = (dir1, dir2) => (dir1 + dir2) % 2 === 0;
 
-window.stopAnim = () => {
+const stopAnimation = () => {
   if (animating || gameover) clearInterval(refreshIntervalId);
   else refreshIntervalId = setInterval(loop, 1000 / fps);
   animating = !animating;
-}
+};
 
 let refreshIntervalId = 0;
 let animating = true;
-const fps = 15;
+const fps = 5;
 
 window.onload = () => {
   canvas = $('#canvas')[0];
